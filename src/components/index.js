ddc56e0c2722a9b47340fc20b67488975a3c10a0
Stephen Belyea <belyea.stephen@gmail.com>
2019-05-29 20:51:01 -0400

    Simple Deck and styles added to index

---
+++ src/components/index.js
@@ -0,0 +1,2 @@
+export { default as Deck } from './Deck';
+export { default as Slide } from './Slide';
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Deck, Slide } from './components';
+import './styles.css';
 
-ReactDOM.render(<h1>Hello there</h1>, document.getElementById("root"));
+const slides = [<Slide>1. Foo</Slide>, <Slide>2. Bar</Slide>, <Slide>3. Foobar</Slide>];
+
+ReactDOM.render(<Deck slides={slides} />, document.getElementById('root'));
