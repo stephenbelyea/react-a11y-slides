36b35a31825e0ab27bcede435f1eb25f52f13861
Stephen Belyea <belyea.stephen@gmail.com>
2019-05-30 21:21:40 -0400

    Next and prev buttons, slide number, and progress bar

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
 
 class Deck extends React.Component {
   constructor(props) {
@@ -8,14 +9,26 @@ class Deck extends React.Component {
     this.state = {
       current: 0
     };
-    this.slidesRef = React.createRef();
+    this.slidesContainer = React.createRef();
   }
 
   getLastSlide() {
     return this.props.slides.length - 1;
   }
 
-  changeSlide = (_, direction = INCREMENT) => {
+  getProgressPercentage() {
+    return ((this.state.current + 1) / this.props.slides.length) * 100;
+  }
+
+  doPrevSlide = () => {
+    this.changeCurrentSlide(DECREMENT);
+  };
+
+  doNextSlide = () => {
+    this.changeCurrentSlide(INCREMENT);
+  };
+
+  changeCurrentSlide = direction => {
     this.setState(
       (state, props) => {
         const prevSlide = state.current;
@@ -29,22 +42,52 @@ class Deck extends React.Component {
         return { current };
       },
       () => {
-        this.slidesRef.current.focus();
+        this.slidesContainer.current.focus();
       }
     );
   };
 
   componentDidMount() {
-    this.slidesRef.current.focus();
+    this.slidesContainer.current.focus();
   }
 
   render() {
     const { slides } = this.props;
     const { current } = this.state;
     return (
-      <div className="deck" aria-live="polite" onClick={this.changeSlide}>
-        <div className="slides" tabIndex="-1" ref={this.slidesRef}>
+      <div className="deck">
+        <section className="slides" tabIndex="-1" ref={this.slidesContainer}>
           {slides[current]}
+        </section>
+        <nav className="controls">
+          <div className="counter" role="status">
+            {current + 1} of {slides.length}
+          </div>
+          <div className="prev-next">
+            <button
+              className="next"
+              type="button"
+              aria-label="Next slide"
+              onClick={this.doNextSlide}
+            >
+              <span className="text-icon" aria-hidden="true">
+                <span className="text">next</span> <span className="icon">&gt;</span>
+              </span>
+            </button>
+            <button
+              className="prev"
+              type="button"
+              aria-label="Previous slide"
+              onClick={this.doPrevSlide}
+            >
+              <span className="text-icon" aria-hidden="true">
+                <span className="icon">&lt;</span> <span className="icon">prev</span>
+              </span>
+            </button>
+          </div>
+        </nav>
+        <div className="progress" role="presentation" aria-hidden="true">
+          <div className="progress-bar" style={{ width: `${this.getProgressPercentage()}%` }} />
         </div>
       </div>
     );
