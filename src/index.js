b25fb0a781527b359d8de29ddc0e5f8644b17bf8
Stephen Belyea <belyea.stephen@gmail.com>
2019-05-31 17:01:26 -0400

    Round out demo slides with some markup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import { Deck, Slide } from './components';
 import './styles.css';
 
-const slides = [<Slide>1. Foo</Slide>, <Slide>2. Bar</Slide>, <Slide>3. Foobar</Slide>];
+const slides = [
+  <Slide>
+    <h1>Metamorphosis</h1>
+    <p>by Franz Kafka</p>
+  </Slide>,
+  <Slide>
+    <p>
+      One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his
+      bed into a horrible vermin.
+    </p>
+  </Slide>,
+  <Slide>
+    <p>
+      He lay on his armour-like back, and if he lifted his head a little he could see his brown
+      belly, slightly domed and divided by arches into stiff sections.
+    </p>
+  </Slide>,
+  <Slide>
+    <p>The bedding was hardly able to cover it and seemed ready to slide off any moment.</p>
+  </Slide>,
+  <Slide>
+    <p>
+      His many legs, pitifully thin compared with the size of the rest of him, waved about
+      helplessly as he looked.
+    </p>
+  </Slide>
+];
 
 ReactDOM.render(<Deck slides={slides} />, document.getElementById('root'));
